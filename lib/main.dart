098c10690b25c4cf5f945e86501067f9359f6219
VivBB <barrigav@uninorte.edu.co>
2022-11-08 21:56:44 -0500

    cambio1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import "package:get/get.dart";
 
 void main() {
   runApp(const MyApp());
@@ -10,25 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Reto 1',
+    return GetMaterialApp(
+      title: 'VJJ DIVISAS SAS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: ' Divisas-VJJ- Converter '),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Aplicaciones Movil - Reto 1'),
     );
   }
 }
@@ -36,38 +24,29 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-final textorigin = TextEditingController();
-final textdestino = TextEditingController();
+final text_origen = TextEditingController();
+final text_destino = TextEditingController();
 
-class calculadora {
+class objetos {
   final titulo;
-  final icono;
+  final Icon1;
   final color;
 
-  calculadora(this.titulo, this.icono, this.color);
+  objetos(this.titulo, this.Icon1, this.color);
 }
 
-String op1 = "USD";
-String op2 = "COP";
+//var modena=["USD", "EUR", "COP"];
+String op1 = "COP";
+String op2 = "USD";
 
-List<String> item2 = <String>["USD", "EUR", "COP"];
-
-List<DropdownMenuItem<String>> item3 = <DropdownMenuItem<String>>[
+List<String> moneda = <String>["USD", "EUR", "COP"];
+List<DropdownMenuItem<String>> moneda2 = <DropdownMenuItem<String>>[
   DropdownMenuItem(
     child: Text("USD"),
     value: "USD",
@@ -82,25 +61,22 @@ List<DropdownMenuItem<String>> item3 = <DropdownMenuItem<String>>[
   ),
 ];
 
-List<calculadora> cal = <calculadora>[
-  calculadora("9", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("8", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("7", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("6", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("5", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("4", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("3", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("2", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("1", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("0", Icon(Icons.abc), Color.fromARGB(255, 171, 248, 240)),
-  calculadora("Limpiar", Icon(Icons.cleaning_services),
-      Color.fromARGB(255, 171, 248, 240)),
-  calculadora(
-      "Calcular", Icon(Icons.calculate), Color.fromARGB(255, 171, 248, 240)),
-];
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<objetos> info = <objetos>[
+    objetos("1", Icon(Icons.abc), Colors.grey),
+    objetos("2", Icon(Icons.abc), Colors.grey),
+    objetos("3", Icon(Icons.abc), Colors.grey),
+    objetos("4", Icon(Icons.abc), Colors.grey),
+    objetos("5", Icon(Icons.abc), Colors.grey),
+    objetos("6", Icon(Icons.abc), Colors.grey),
+    objetos("7", Icon(Icons.abc), Colors.grey),
+    objetos("8", Icon(Icons.abc), Colors.grey),
+    objetos("9", Icon(Icons.abc), Colors.grey),
+    objetos("0", Icon(Icons.abc), Colors.grey),
+    objetos("Limpiar", Icon(Icons.clear_rounded), Colors.blueAccent),
+    objetos("Calcular", Icon(Icons.calculate), Colors.blueAccent),
+  ];
 
   void _incrementCounter() {
     setState(() {
@@ -111,93 +87,118 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(15),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+          padding: EdgeInsets.all(18),
           child: Column(
             children: [
-              Row(children: [
-                Text("origin"),
-                DropdownButton(
-                    value: op1,
-                    items: item3,
-                    onChanged: (String? x) {
-                      setState(() {
-                        op1 = x.toString();
-                      });
-                    }),
-                Text("Destino"),
-                DropdownButton(
-                    value: op2,
-                    items: item3,
-                    onChanged: (String? x) {
-                      setState(() {
-                        op2 = x.toString();
-                      });
-                    }),
-              ]),
-              TextField(
-                controller: textorigin,
-                decoration: InputDecoration(labelText: "Origen", hintText: "0"),
+              Row(
+                children: [
+                  Text("   De    "),
+                  //esta es la lista de monedas >> Lista de doble convercion de manera iterativa
+                  DropdownButton(
+                      value: op1,
+                      items: moneda.map((e) {
+                        return DropdownMenuItem(value: e, child: Text(e));
+                      }).toList(),
+                      onChanged: (String? x) {
+                        setState(() {
+                          op1 = x.toString();
+                        });
+                      }),
+
+                  Text("   a   "),
+                  DropdownButton
+                      // esta es la lista 2 de monedas
+                      (
+                          value: op2,
+                          items: moneda2,
+                          onChanged: (String? x) {
+                            setState(() {
+                              op2 = x.toString();
+                            });
+                          }),
+                ],
               ),
               TextField(
-                controller: textdestino,
-                decoration:
-                    InputDecoration(labelText: "Destino", hintText: "0"),
+                controller: text_origen,
+                decoration: InputDecoration(labelText: " De", hintText: "0"),
               ),
+              TextField(
+                controller: text_destino,
+                decoration: InputDecoration(labelText: "a", hintText: "0"),
+              ),
+              Divider(),
               Expanded(
                 child: GridView.builder(
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    //Aqui van las columnas
                     crossAxisCount: 3,
                   ),
-                  itemCount: cal.length,
+                  itemCount: info.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                        color: cal[index].color,
+                    return Container(
+                      width: 10,
+                      child: Card(
+                        color: info[index].color,
                         child: ListTile(
                           title: Center(
-                            child: index > 9
-                                ? cal[index].icono
-                                : Text(
-                                    cal[index].titulo,
+                            child: index == 10 || index == 11
+                                ? info[index].Icon1
+                                : Text(info[index].titulo,
                                     style: TextStyle(
-                                        fontSize: 30,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold)),
                           ),
                           onTap: () {
-                            print(cal[index].titulo);
+                            print(info[index].titulo);
 
                             if (index < 10) {
-                              textorigen.text =
-                                  textorinen.text + cal[index].titulo;
+                              text_origen.text =
+                                  text_origen.text + info[index].titulo;
                             } else if (index == 10) {
-                              textorigen.text = "";
-                              textdestino.text = "";
+                              text_origen.text = "";
+                              text_destino.text = "";
                             } else {
                               if (op1 == "USD" && op2 == "COP") {
-                                textdestino.text =
-                                    (double.parse(textorigin.text) * 5000)
+                                text_destino.text =
+                                    (double.parse(text_origen.text) * 4974)
                                         .toString();
                               } else if (op1 == "COP" && op2 == "USD") {
-                                textdestino.text =
-                                    (double.parse(textorigin.text) / 5000)
+                                text_destino.text =
+                                    (double.parse(text_origen.text) / 4974)
                                         .toString();
-                            } (op1 == "EUR" && op2 == "COP") {
-                                textdestino.text =
-                                    (double.parse(textorigin.text) * 5050)
-                                        .toString();}
-                          } 
-                        ));
+                              } else if (op1 == "EUR" && op2 == "COP") {
+                                text_destino.text =
+                                    (double.parse(text_origen.text) * 5075)
+                                        .toString();
+                              } else if (op1 == "COP" && op2 == "EUR") {
+                                text_destino.text =
+                                    (double.parse(text_origen.text) / 5075)
+                                        .toString();
+                              } else if (op1 == "EUR" && op2 == "USD") {
+                                text_destino.text =
+                                    (double.parse(text_origen.text) * 0.99)
+                                        .toString();
+                              } else if (op1 == "USD" && op2 == "EUR") {
+                                text_destino.text =
+                                    (double.parse(text_origen.text) / 0.99)
+                                        .toString();
+                              } else {
+                                text_destino.text = text_origen.text;
+                              }
+                            }
+                          },
+                        ),
+                      ),
+                    );
                   },
                 ),
               )
             ],
-          ),
-        )
-        // This trailing comma makes auto-formatting nicer for build methods.
-        );
+          )),
+    );
   }
 }
